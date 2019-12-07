ca777f8e154cdad1a89734c294c7ca5e06e99741
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2019-12-07 14:45:30 +0100

    Add solution for day06 part1

---
+++ src/bin/day06.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn solve_p1(edges: &[(&str, &str)]) -> usize {
+    let mut orbit_map: HashMap<&str, &str> = HashMap::new();
+    for (orbited, orbiter) in edges {
+        orbit_map.insert(orbiter, orbited);
+    }
+
+    let mut count = 0;
+    for (_, orbiter) in edges {
+        let mut orbited = orbiter;
+        while orbit_map.contains_key(orbited) {
+            count += 1;
+            orbited = orbit_map.get(orbited).unwrap();
+        }
+    }
+
+    count
+}
+
+fn main() {
+    let input = fs::read_to_string("./input/day06.txt").unwrap();
+    let edges: Vec<(&str, &str)> = input
+        .split('\n')
+        .filter(|ele| !ele.is_empty())
+        .map(|ele| {
+            let edge: Vec<&str> = ele.split(')').collect();
+            (edge[0], edge[1])
+        })
+        .collect();
+
+    println!("p1: {}", solve_p1(&edges));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_p1() {
+        assert_eq!(
+            solve_p1(&[
+                ("COM", "B"),
+                ("B", "C"),
+                ("C", "D"),
+                ("D", "E"),
+                ("E", "F"),
+                ("B", "G"),
+                ("G", "H"),
+                ("D", "I"),
+                ("E", "J"),
+                ("J", "K"),
+                ("K", "L"),
+            ]),
+            42
+        );
+    }
+}

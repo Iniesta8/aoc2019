af4c706e2de1aead308632ef785bd831c2ccc04e
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2019-12-08 14:29:25 +0100

    Add solution for day07 part2

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -20,11 +20,42 @@ fn run_amplifiers(cpu: &IntCodeCpu) -> i32 {
         .unwrap()
 }
 
+fn run_amplifiers_with_feedback(cpu: &IntCodeCpu) -> i32 {
+    (5..10)
+        .permutations(5)
+        .map(|phase_settings| {
+            let mut amplifiers: Vec<IntCodeCpu> = phase_settings
+                .iter()
+                .map(|phase_setting| {
+                    let mut amplifier = cpu.clone();
+                    amplifier.input.push_back(*phase_setting);
+                    amplifier
+                })
+                .collect();
+
+            let mut output = 0;
+            while amplifiers.first().unwrap().running {
+                for amplifier in &mut amplifiers {
+                    amplifier.input.push_back(output);
+                    if let Some(out) = amplifier.run_until_output() {
+                        output = out;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            output
+        })
+        .max()
+        .unwrap()
+}
+
 fn main() {
     let input = fs::read_to_string("./input/day07.txt").unwrap();
     let cpu = IntCodeCpu::from_code(&input);
 
     println!("p1: {}", run_amplifiers(&cpu));
+    println!("p2: {}", run_amplifiers_with_feedback(&cpu));
 }
 
 #[cfg(test)]
@@ -60,6 +91,29 @@ mod tests {
                 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
             )),
             65210
+        );
+    }
+
+    #[test]
+    fn test_run_amplifiers_with_feedback() {
+        assert_eq!(
+            run_amplifiers_with_feedback(&IntCodeCpu::from_code(
+                "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+                27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+            )),
+            139629729
+        );
+    }
+
+    #[test]
+    fn test_run_amplifiers_with_feedback2() {
+        assert_eq!(
+            run_amplifiers_with_feedback(&IntCodeCpu::from_code(
+                "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+                -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+                53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+            )),
+            18216
         );
     }
 }

b8db985a42918133d643dd73b0a155c77150690d
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2019-12-09 21:42:31 +0100

    Add solution for day09

---
+++ src/bin/day09.rs
@@ -0,0 +1,19 @@
+use aoc2019::intcode::IntCodeCpu;
+use std::fs;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let code = fs::read_to_string("./input/day09.txt").unwrap();
+
+    let mut cpu = IntCodeCpu::from_code(&code);
+    cpu.input.push_back(1);
+    cpu.run();
+    println!("p1: {}", cpu.output.pop_back().unwrap());
+
+    cpu = IntCodeCpu::from_code(&code);
+    cpu.input.push_back(2);
+    cpu.run();
+    println!("p2: {}", cpu.output.pop_back().unwrap());
+
+    Ok(())
+}
